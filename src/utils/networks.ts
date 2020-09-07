baec0c56ec6d2a9c7ab5d7fce086808296f4921e
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-09-08 01:54:30 +0900

    feat: use redirectPage to share twitter with image

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -45,8 +45,16 @@ export const issueTwitterIntent = (
   let rawText = `座席「${ticketSheetName}」のチケットを発券しました！
       
 「Aqours Back In 5th LoveLive! ～Next SPARKLING!!～」に向けて、レプリカチケットを発券しよう！`;
-  if (rawText) {
+
+  let url = encodeURIComponent(`https://replica-ticket.web.app/`);
+
+  // 画像つき共有の場合は、TwitterCard用OGPを設定しない
+  if (mediaUrl) {
     rawText += `\n${mediaUrl}`;
+    const redirectUrl = `https://replica-ticket.web.app`;
+    url = encodeURIComponent(
+      `https://api.sokontokoro-factory.net/twitter/share?redirectUrl=${redirectUrl}`
+    );
   }
   let text = encodeURIComponent(rawText);
 
@@ -55,8 +63,6 @@ export const issueTwitterIntent = (
     "そこんところ工房",
     "レプリカチケット発券機",
   ].join(",");
-
-  const url = encodeURIComponent(`https://replica-ticket.web.app/`);
 
   window.open(`${baseUrl}?text=${text}&hashtags=${hashtags}&url=${url}`);
 };
